6e4ace1b2d234446e76d1be21a0651b28a7af2cd
xpero39 <peter.vreca@gmail.com>
2017-06-20 22:05:00 +0200

    Initial commit.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var app = new Vue ({
+                el: '#app', 
+                data: { 
+                    message:'New path',
+                    goals: ['Learn VueJS','Learn Algorithms','Learn best coding practices'],
+                },
+                
+            })
