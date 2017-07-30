644c249aac13680ef28e80ae60c794a90ea71ab0
xpero39 <peter.vreca@gmail.com>
2017-07-30 22:36:06 +0200

    Axios insert.

--- js/app.js
+++ js/app.js
@@ -5,44 +5,88 @@ var app = new Vue ({
                     pmessage:'New project',
                     hmessage:'New habit',
                     fmessage:'New fear',
-                    //Usable if you want to load data into VueJS
-                    goals: ['Learn VueJS','Learn Algorithms','Learn best coding practices', 'Learn German'],
+                    //Usable if you want to load data into VueJS 
+                   /*  goals: ['Learn VueJS','Learn Algorithms','Learn best coding practices', 'Learn German'],
                     projects: [],
                     habits: [],
-                    fears: [],
+                    fears: [], */
                 },
                 
                 methods: {
                     insert: function(section) {
-                        
-                        //axios post
-                        axios({
-                        method: 'post',
-                        url: 'insert.php',
-                        data: {
-                           section: section,
-                           
+                        switch (section) {
+                            case 'goal':
+                                //axios post
+                                axios({
+                                    method: 'post',
+                                    url: 'insert.php',
+                                    data: {
+                                        section: section,
+                                        data: gmessage,
+                                    }
+                                    .then(function (response) {
+                                        console.log(response);
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    })
+                                });
+                                break;
+                            case 'project':
+                                //axios post
+                                axios({
+                                    method: 'post',
+                                    url: 'insert.php',
+                                    data: {
+                                        section: section,
+                                        data: pmessage,
+                                    }
+                                    .then(function (response) {
+                                        console.log(response);
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    })
+                                });
+                                break;
+                            case 'habit':
+                                //axios post
+                                axios({
+                                    method: 'post',
+                                    url: 'insert.php',
+                                    data: {
+                                        section: section,
+                                        data: hmessage,
+                                    }
+                                    .then(function (response) {
+                                        console.log(response);
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    })
+                                });
+                                break;
+                            case 'fear':
+                                //axios post
+                                axios({
+                                    method: 'post',
+                                    url: 'insert.php',
+                                    data: {
+                                        section: section,
+                                        data: fmessage,
+                                    }
+                                    .then(function (response) {
+                                        console.log(response);
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    })
+                                });
+                                break;
+                            default:
+                                // No default action
+                                break;
                         }
-                        .then(function (response) {
-                            console.log(response);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        })
-                        });
-                        
-                        //old jquery ajax
-                        /* $.ajax({
-                            type: "POST",
-                            url: "insert.php",
-                            data: ''+string+'',
-                            success : function(text){
-                                if (text != ""){
-                                    formGenericSuccess(text,formID);
-                                }
-                            }
-                        }); */
-
                     },
 
                 }
