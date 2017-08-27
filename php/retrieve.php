add04c994e21166e53be36807bd6b0afca4009d3
xpero39 <peter.vreca@gmail.com>
2017-08-27 10:14:09 +0200

    SQL update.

--- php/retrieve.php
+++ php/retrieve.php
@@ -20,11 +20,11 @@ switch($data){
 		$fears = $conn->query($sql) or trigger_error($conn->error. " [$sql]");
 		break;
 	case "active":
-		$sql = "SELECT f_title, g_title, h_title, p_title FROM fears, habits, projects, goals"; // not working properly
+		$sql = "SELECT f_title, g_title, h_title, p_title FROM fears, habits, projects, goals WHERE 'active = 1'"; // CHANGE TO JOIN
 		$active = $conn->query($sql) or trigger_error($conn->error. " [$sql]");
 		break;
 	case "done":
-		$sql = "SELECT f_title FROM fears";
+		$sql = "SELECT f_title, g_title, h_title, p_title FROM fears, habits, projects, goals WHERE 'done = 1'"; // CHANGE TO JOIN
 		$done = $conn->query($sql) or trigger_error($conn->error. " [$sql]");
 		break;
 	default:
